fe6250787db7558e36562ba29b46f6b38c06f2a2
Paul Stamp <paul@nonatomic.co.uk>
2023-11-19 18:06:18 +0000

    Validated that more the statemachine can transition more than one state per frame

---
+++ Assets/Package/Samples/Example/Source/StateOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using VisualStateMachine;
+
+namespace Samples.Example.Source
+{
+	public class StateOne : State
+	{
+		[Transition]
+		public event Action Complete;
+		
+		public override void Enter()
+		{
+			Complete?.Invoke();
+		}
+
+		public override void Update()
+		{
+			
+		}
+
+		public override void Exit()
+		{
+			
+		}
+	}
+}
---
+++ Assets/Package/Samples/Example/Source/StateTwo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using VisualStateMachine;
+
+namespace Package.Samples.Example.Source
+{
+	public class StateTwo : State
+	{
+		[Transition]
+		public event Action Complete;
+		
+		public override void Enter()
+		{
+			Complete?.Invoke();
+		}
+
+		public override void Update()
+		{
+			
+		}
+
+		public override void Exit()
+		{
+			
+		}
+	}
+}
