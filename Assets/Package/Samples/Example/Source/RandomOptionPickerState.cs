08c211a6d0b89ac83818217eb44e6241664fb4fc
Paul Stamp <paul@nonatomic.co.uk>
2023-11-20 00:43:51 +0000

    Added more test nodes.

---
+++ Assets/Package/Samples/Example/Source/RandomOptionPickerState.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using VisualStateMachine;
+using Random = UnityEngine.Random;
+
+namespace Package.Samples.Example.Source
+{
+	public class RandomOptionPickerState : State
+	{
+		[Transition]
+		public event Action OptionOne;
+
+		[Transition] 
+		public event Action OptionTwo;
+		
+		public override void Enter()
+		{
+			if (Random.value >= 0.5f)
+			{
+				OptionOne.Invoke();
+			}
+			else
+			{
+				OptionTwo.Invoke();
+			}
+		}
+
+		public override void Update()
+		{
+			
+		}
+
+		public override void Exit()
+		{
+			
+		}
+	}
+}
--- Assets/Package/Samples/Example/Source/RotationState.cs
+++ Assets/Package/Samples/Example/Source/RotationState.cs
@@ -1,19 +1,38 @@
-﻿using VisualStateMachine;
+﻿using System;
+using UnityEngine;
+using VisualStateMachine;
 
 namespace Example
 {
 	public class RotationState : State
 	{
+		[Transition]
+		public event Action Complete;
+		
+		[SerializeField] private float _speed = 1f;
+		[SerializeField] private Vector3 _axis = Vector3.forward;
+		[SerializeField] private float _duration = 1f;
+		
 		private StateMachineReferences _references;
-
+		private float _entryTime;
+		
 		public override void Enter()
 		{
 			_references = this.Controller.GetComponent<StateMachineReferences>();
+			_entryTime = Time.time;
 		}
 
 		public override void Update()
 		{
-			_references.Cube.Rotate(0, 0, 1);
+			var speed = _speed * Time.deltaTime;
+			var axis = _axis.normalized * _speed;
+			
+			_references.Cube.Rotate(axis);
+
+			if (Time.time - _entryTime > _duration)
+			{
+				Complete?.Invoke();
+			}
 		}
 
 		public override void Exit()
