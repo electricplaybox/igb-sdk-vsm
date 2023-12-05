c71c8e5cb6b6525150b32438f2bbe39a66ba6c05
Paul Stamp <paul@nonatomic.co.uk>
2023-12-05 13:20:55 +0000

    Fix for property container width not spanning full node width when there are no properties

---
+++ Assets/Testing/TestingState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using VisualStateMachine.Attributes;
+using VisualStateMachine.States;
+
+namespace Testing
+{
+	public class TestingState : State
+	{
+		[Transition] 
+		public event Action MyTransition;
+		
+		public override void EnterState()
+		{
+			
+		}
+
+		public override void UpdateState()
+		{
+			
+		}
+
+		public override void ExitState()
+		{
+			
+		}
+	}
+}
